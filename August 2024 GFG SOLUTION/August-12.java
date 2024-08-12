0738e021ab0c0573986a1429492132b626fa5594
Svapnil Rathod <144311070+Svapnil16@users.noreply.github.com>
2024-08-12 21:34:58 +0530

    Create August-12.java

---
+++ August 2024 GFG SOLUTION/August-12.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public int SumofMiddleElements(int[] arr1, int[] arr2) {
+        // code here
+        PriorityQueue<Integer>pq = new PriorityQueue<>();
+        for(int a : arr1)
+        pq.add(a);
+        for(int a : arr2)
+        pq.add(a);
+        int combination[] = new int[2*arr1.length];
+        int index = 0;
+        while(!pq.isEmpty())
+        combination[index++] = pq.poll();
+        
+        return combination[combination.length/2] + combination[(combination.length/2)-1];
+    }
+}
+
+//{ Driver Code Starts.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume newline
+        while (t-- > 0) {
+            String[] input1 = sc.nextLine().split(" ");
+            int[] arr = new int[input1.length];
+            for (int i = 0; i < input1.length; i++) {
+                arr[i] = Integer.parseInt(input1[i]);
+            }
+
+            String[] input2 = sc.nextLine().split(" ");
+            int[] brr = new int[input2.length];
+            for (int i = 0; i < input2.length; i++) {
+                brr[i] = Integer.parseInt(input2[i]);
+            }
+
+            Solution ob = new Solution();
+            int res = ob.SumofMiddleElements(arr, brr);
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
